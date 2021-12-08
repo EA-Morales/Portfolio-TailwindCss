bac0f330b82cfab767b80028638f969801244473
Agustin <eduardo.agustin.morales@gmail.com>
2021-12-08 17:47:18 -0300

    Updates

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: {
+    enabled: true,
+    content: ['./Public/*.html'],
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+};
